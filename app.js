32da6574266e376bc712e21961c4266eca2e8c0a
DimaDovgan <89789155+DimaDovgan@users.noreply.github.com>
2023-06-09 12:33:33 +0300

    0906

--- app.js
+++ app.js
@@ -6,27 +6,26 @@ var logger = require('morgan');
 // const fileUpload=require('express-fileupload');
 
 
-var indexRouter = require('./routes/api/leson');
-var usersRouter = require('./routes/api/auth');
-const cors = require('cors');
+// var indexRouter = require('./routes/api/leson');
+// var usersRouter = require('./routes/api/auth');
+// const cors = require('cors');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(cors({
-  origin: '*'
-}));
+// // view engine setup
 
-// app.use(fileUpload({}))
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/leson', indexRouter);
-app.use('/api/users', usersRouter);
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.use(cors({
+//   origin: '*'
+// }));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/api/leson', indexRouter);
+// app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
