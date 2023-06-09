6d50c71e606c96173c34d49aaf70d4a4b4e5f570
DimaDovgan <89789155+DimaDovgan@users.noreply.github.com>
2023-06-09 17:15:50 +0300

    add ghghdghdgfdfdfh

--- app.js
+++ app.js
@@ -45,21 +45,30 @@ app.get('/test', (req, res) => {
 })
 
 // // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   //res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-  res.json(err.message);
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+//   res.json(err.message);
+// });
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+  const {status = 500, message = "Server error"} = err;
+  res.status(status).json({message})
+})
 
 module.exports = app;
 
