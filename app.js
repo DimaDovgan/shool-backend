86533a66395d6b83f3a27a3c061b712ea9b83f57
DimaDovgan <89789155+DimaDovgan@users.noreply.github.com>
2023-06-09 13:29:33 +0300

    add 1329s

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/test', (req, res) => {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  console.log("cors err")
+  console.log("req",req)
   next(createError(404));
 });
 
