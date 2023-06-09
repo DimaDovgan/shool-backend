d60e082c1bbb1cd7288791d9bd51bce2a55c1b8c
DimaDovgan <89789155+DimaDovgan@users.noreply.github.com>
2023-06-09 17:49:00 +0300

    O32K

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,12 +4,12 @@ const ctrl = require("../../controllers/auth/index");
 const {auth,upload}=require("../../middlewares")
 router.post('/signup', ctrl.register);
 router.post('/login', ctrl.login);
-router.post('/googleLogin', ctrl.googleLogin);
+//router.post('/googleLogin', ctrl.googleLogin);
 router.get('/current', auth, ctrl.current);
 router.post('/logout', auth, ctrl.logout);
 router.patch('/', auth, ctrl.subscriptionUpdate);
-router.get('/google', ctrl.googleAuth);
-router.get('/google-redirect', ctrl.googleRedirect);
+//router.get('/google', ctrl.googleAuth);
+//router.get('/google-redirect', ctrl.googleRedirect);
 
 
 
