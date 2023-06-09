a12d13934f21c4fcd088bd188c2987204603a691
DimaDovgan <89789155+DimaDovgan@users.noreply.github.com>
2023-06-09 17:05:48 +0300

    add 11112

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-  const createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 // // const fileUpload=require('express-fileupload');
 
 
@@ -10,7 +10,7 @@
 // // var usersRouter = require('./routes/api/auth');
 //  const cors = require('cors');
 
-// var app = express();
+const app = express();
 
 // // // view engine setup
 
@@ -33,65 +33,63 @@
 //   );
 //   next();
 // })
-// // app.use(logger('dev'));
-// // app.use(express.json());
-// // app.use(express.urlencoded({ extended: false }));
-// // app.use(cookieParser());
-// // app.use(express.static(path.join(__dirname, 'public')));
+ app.use(logger('dev'));
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+ app.use(express.static(path.join(__dirname, 'public')));
 // // app.use('/api/leson', indexRouter);
 // // app.use('/api/users', usersRouter);
-// app.get('/test', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// // catch 404 and forward to error handler
-// // app.use(function(req, res, next) {
-// //   console.log("req",req)
-// //   next(createError(404));
-// // });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   //res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
-
-
-
-
-
-
-
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-  console.log("OKK")
+app.get('/test', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/test', (req, res) => {
-  console.log("OKK TEST")
-  res.send('Hello World! TEST')
-})
 
-//catch 404 and forward to error handler
+// // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  console.log("req",req)
   next(createError(404));
 });
 
+// // error handler
 app.use(function(err, req, res, next) {
-  console.log("error")
+  // set locals, only providing error in development
   res.locals.message = err.message;
+  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.json(err.message);
+  res.render('error');
 });
 
 module.exports = app;
+
+
+
+
+
+
+
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (req, res) => {
+//   console.log("OKK")
+//   res.send('Hello World!')
+// })
+// app.get('/test', (req, res) => {
+//   console.log("OKK TEST")
+//   res.send('Hello World! TEST')
+// })
+
+// //catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// app.use(function(err, req, res, next) {
+//   console.log("error")
+//   res.locals.message = err.message;
+//   res.status(err.status || 500);
+//   res.json(err.message);
+// });
+
+// module.exports = app;
